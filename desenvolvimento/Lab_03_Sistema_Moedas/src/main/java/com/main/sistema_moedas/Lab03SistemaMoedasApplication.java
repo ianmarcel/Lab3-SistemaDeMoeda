c77f8e95151c7c23ff8f502ed005474701201cdd
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2022-04-29 11:50:16 -0300

    Configuração inicial do codigo

---
+++ desenvolvimento/Lab_03_Sistema_Moedas/src/main/java/com/main/sistema_moedas/Lab03SistemaMoedasApplication.java
@@ -0,0 +1,13 @@
+package com.main.sistema_moedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab03SistemaMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab03SistemaMoedasApplication.class, args);
+	}
+
+}
