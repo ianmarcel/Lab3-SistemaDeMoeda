f209422166fb56035ac5b341eecb0fe7d3b932a2
ianmarcel <ianmarcelcampos@gmail.com>
2022-05-05 19:22:03 -0300

    mudanca controller aluno empresa ,criacao do role repository

--- desenvolvimento/Lab_03_Sistema_Moedas/src/main/java/com/main/sistema_moedas/controller/AlunoController.java
+++ desenvolvimento/Lab_03_Sistema_Moedas/src/main/java/com/main/sistema_moedas/controller/AlunoController.java
@@ -1,28 +1,41 @@
 package com.main.sistema_moedas.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.main.sistema_moedas.model.Conta;
 import com.main.sistema_moedas.model.Endereco;
 import com.main.sistema_moedas.model.usuario.Aluno;
+import com.main.sistema_moedas.model.usuario.Role;
+import com.main.sistema_moedas.repository.RoleRepository;
 import com.main.sistema_moedas.repository.UsuarioRepository;
 
 @Controller
 @RequestMapping("/aluno")
 public class AlunoController {
-	
+
+	@Autowired
+	private RoleRepository rRepository;
 	@Autowired
 	private UsuarioRepository uRepository;
+
 	@PostMapping("/new")
-	public String novo(Aluno a,Endereco e) {
+	public String novo(Aluno a, Endereco e) {
+		Role admin = rRepository.findByNameRole("ROLE_ADMIN").orElse(new Role("ROLE_ADMIN"));
+		Role aluno = rRepository.findByNameRole("ROLE_ALUNO").orElse(new Role("ROLE_ALUNO"));
+		List<Role> listaderoles = new ArrayList<>();
+		listaderoles.add(admin);
+		listaderoles.add(aluno);
+		a.setRoles(listaderoles);
 		a.setEndereco(e);
 		a.setConta(new Conta());
 		uRepository.save(a);
-	   return  ("redirect:/aluno/");
+		return ("redirect:/aluno/");
 	}
 
 }
