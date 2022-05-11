08bc473ab187f1827894cef60a865ced4797b1fb
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2022-05-11 19:31:24 -0300

    implementação (transação e adicionar entidades)

--- desenvolvimento/Lab_03_Sistema_Moedas/src/main/java/com/main/sistema_moedas/model/usuario/Professor.java
+++ desenvolvimento/Lab_03_Sistema_Moedas/src/main/java/com/main/sistema_moedas/model/usuario/Professor.java
@@ -3,7 +3,9 @@ package com.main.sistema_moedas.model.usuario;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
+import com.main.sistema_moedas.model.Conta;
 import com.main.sistema_moedas.model.Instituicao;
 
 @Entity
@@ -16,12 +18,17 @@ public class Professor extends Usuario {
 	private String departamento;
 	@ManyToOne(targetEntity = Instituicao.class)
 	private Instituicao instituicao;
+	@OneToOne
+	private Conta conta;
 
-	
-	public Professor() {
+
+	public Conta getConta() {
+		return this.conta;
 	}
 
-
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
 
 	public String getCpf() {
 		return cpf;
